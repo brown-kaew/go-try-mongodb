df2f331093f220f7fa3df45881269d6d9df1a23f
Kaew Tibkham <kaewtibkham@gmail.com>
2023-08-27 01:44:17 +0700

    prepare test date in mongo

--- mongo-init.js
+++ mongo-init.js
@@ -8,3 +8,13 @@ db.createUser({
         },
     ],
 });
+
+db = new Mongo().getDB("mydb");
+
+db.createCollection("user", { capped: false });
+
+db.user.insert([
+    { id: 1, name: "Kaew" },
+    { id: 2, name: "Arin" },
+    { id: 3, name: "John" },
+]);
