83aeb04ba1aaa4a418a30d69e20f328d5c55dc09
Kaew Tibkham <kaewtibkham@gmail.com>
2023-08-27 00:49:14 +0700

    add redis

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.21
 require (
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/labstack/gommon v0.4.0
+	github.com/redis/go-redis/v9 v9.1.0
 	go.mongodb.org/mongo-driver v1.12.1
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
---
+++ user/redis.go
@@ -0,0 +1,64 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type SimpleRedis interface {
+	// Close()
+	Get(key string) *User
+	Put(key string, value *User) error
+}
+
+type simpleRedis struct {
+	ctx    context.Context
+	client *redis.Client
+	close  func()
+}
+
+func NewSimpleRedis() SimpleRedis {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+		Protocol: 3,  // specify 2 for RESP 2 or 3 for RESP 3
+	})
+	return &simpleRedis{
+		ctx:    context.Background(),
+		client: client,
+		// close:  func() { client.Close() },
+	}
+}
+
+func (simpleRedis *simpleRedis) Get(key string) *User {
+	value, err := simpleRedis.client.Get(simpleRedis.ctx, key).Result()
+	if err == redis.Nil {
+		log.Printf("%s does not exist", key)
+	} else if err != nil {
+		log.Printf("error=%v", err)
+	} else {
+		var user User
+		json.Unmarshal([]byte(value), &user)
+		return &user
+	}
+	return nil
+}
+
+func (simpleRedis *simpleRedis) Put(key string, user *User) error {
+	value, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	err = simpleRedis.client.Set(simpleRedis.ctx, key, string(value), 1*time.Minute).Err()
+	if err != nil {
+		return err
+	}
+	return err
+}
